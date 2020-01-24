1303e0202cc81be391c3676a99003865dcf25bc4
Slava <ukrslava@yandex.ua>
2020-01-24 10:24:42 +0200

    1

--- BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration_adv.h
+++ BTT SKR V1.3/firmware/Marlin-bugfix-2.0.x/Marlin/Configuration_adv.h
@@ -519,12 +519,12 @@
 // @section homing
 
 // Homing hits each endstop, retracts by these distances, then does a slower bump.
-#define X_HOME_BUMP_MM 5
-#define Y_HOME_BUMP_MM 5
-#define Z_HOME_BUMP_MM 2
+#define X_HOME_BUMP_MM 2
+#define Y_HOME_BUMP_MM 2
+#define Z_HOME_BUMP_MM 1
 #define HOMING_BUMP_DIVISOR { 2, 2, 4 }  // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 #define QUICK_HOME                       // If homing includes X and Y, do a diagonal move initially
-#define HOMING_BACKOFF_MM { 1, 1, 1 }  // (mm) Move away from the endstops after homing
+//#define HOMING_BACKOFF_MM { 1, 1, 1 }  // (mm) Move away from the endstops after homing
 
 // When G28 is called, this option will make Y home before X
 //#define HOME_Y_BEFORE_X
